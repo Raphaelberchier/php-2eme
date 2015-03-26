f2bd6883d2a06943108bf5d50d38ea94ab083262
Raphael Berchier <raphael.berchier@cpnv.ch>
2015-03-26 09:23:42 +0100

    Un message

---
+++ chapitre1/hello.php
@@ -0,0 +1,32 @@
+ <!DOCTYPE html>
+ 
+ <?php
+ /**
+  * Ce script est notre premier, y en a pas deux comme lui
+  * 
+  * @author cly <clyavasad.ch>
+  * @date 2015.02.19
+  */
+  ?>
+  <html lang="fr">
+     <head>
+         <meta charset="utf-8">
+         <title> Ma première page</title>
+     </head>
+     
+     <body>
+        
+        tant qu'on n'est pas dans du code php...
+        <br>
+        Ces lignes seront imprimées telles quelles
+        <br>
+        <?php
+
+        echo 'Bonjour de la commande echo';
+
+        ?>
+        <br>
+        Et encore une autre ligne
+        
+     </body>
+ </html>
